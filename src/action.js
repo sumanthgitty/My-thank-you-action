89a736f1d55b3e9a02fafae22c49580955f26a1c
sumanthgitty <sumanthsagar07@gmail.com>
2024-11-28 19:06:15 +0530

    added saying thannk you to pull request

--- src/action.js
+++ src/action.js
@@ -1,5 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
-    console.log('Hello, world!');
+    const GITHUB_TOKEN = core.getInput('GITHUB_tokrn');
+    const octokit = github.getOctokit(GETHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+
+    await octokit.issues.rest.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: 'Thank you for submitting a pull request! we will review it as soon as we can'
+    })
+
 }
+
 
 run();
