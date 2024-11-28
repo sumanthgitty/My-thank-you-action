4fdd3f61db1b83e2dcb811903621b38f87fd84b9
sumanthgitty <sumanthsagar07@gmail.com>
2024-11-28 19:16:33 +0530

    custom action to saying thannk you to pull request

--- src/action.js
+++ src/action.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_tokrn');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
